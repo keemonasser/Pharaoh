e107d3f2117c3a38b6d2e858b1a0c13375856398
keemonasser <keemonasser@gmail.com>
2026-02-24 22:54:18 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+[VERSION]
+LatestVersion=2
+MinimalVersion=1
+OwnVersion=1
+
+[DOWNLOAD]
+server1=http://cf-pharaoh.fun/cf/download/
+
+[SERVER]
+COUNT=1
+IP1=51.195.202.246
+PORT1=13008
+
+[HGW]
+PORT=16666
+SERVER1=51.195.202.246
+POD1=30	
